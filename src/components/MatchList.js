0b4980af0926703d06b6a0faa43e6ab57bf25077
kazvee <kayvee123@gmail.com>
2023-08-29 17:36:44 -0600

    Add MatchList component

---
+++ src/components/MatchList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Match from "./Match";
+
+const MatchList = () => {
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      <Match />
+    </section>
+  );
+};
+
+export default MatchList;
