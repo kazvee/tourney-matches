aa04e3346ed4230c82bc1d40f1884a16874069da
kazvee <kayvee123@gmail.com>
2023-09-01 15:16:40 -0600

    Import & use helper functions inside the PlayerList component

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,8 +2,11 @@ import React from "react";
 import Player from "./Player";
 import playerData from '../data/playerData';
 import matchData from '../data/matchData';
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 const PlayerList = (props) => {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
