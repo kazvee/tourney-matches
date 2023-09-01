8e7166329fbe70d2e52a7e183eae5006e30cce38
kazvee <kayvee123@gmail.com>
2023-09-01 14:18:14 -0600

    Add import for matchData to the MatchList component

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Match from "./Match";
+import matchData from "../data/matchData";
 
-const MatchList = () => {
+const MatchList = (props) => {
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
