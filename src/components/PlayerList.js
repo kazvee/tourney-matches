91268b394a592578dad549004498c07854cd0203
kazvee <kayvee123@gmail.com>
2023-09-01 14:13:04 -0600

    Add import for playerData to the PlayerList component

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Player from "./Player";
+import playerData from '../data/playerData';
+import matchData from '../data/matchData';
 
-const PlayerList = () => {
+const PlayerList = (props) => {
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
