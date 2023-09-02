987b0d66bd000ad7e3b511d497eb44becb418cfd
kazvee <kayvee123@gmail.com>
2023-09-01 18:24:00 -0600

    Pass one player as props from PlayerList to the Player component

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -5,14 +5,17 @@ import matchData from '../data/matchData';
 import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 const PlayerList = (props) => {
+
   const playerDataArray = preparePlayerData(playerData);
   const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const onePlayer = parsedPlayerData[0];
+
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player />
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
     </section>
   );
-};
+}
 
 export default PlayerList;
