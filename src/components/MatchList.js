66b35973348f09dac7d9260c9ee7b97f6bfe379e
kazvee <kayvee123@gmail.com>
2023-09-01 18:20:34 -0600

    Pass one match as props from MatchList to the Match component

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -3,12 +3,15 @@ import Match from "./Match";
 import matchData from "../data/matchData";
 
 const MatchList = (props) => {
+
+  const oneMatch = matchData[0];
+
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match />
+      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
     </section>
   );
-};
+}
 
 export default MatchList;
