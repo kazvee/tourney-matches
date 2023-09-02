10b72213a87441fd0c20d373bd3f2df801904071
kazvee <kayvee123@gmail.com>
2023-09-01 18:23:08 -0600

    Refactor Match component to receive props to render one match

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,16 +1,15 @@
 import React from "react";
 
-const Match = () => {
+const Match = (props) => {
   return (
     <article className="Match">
-      <h1>
-        Joe <span>vs</span> Bob
-      </h1>
+      <h1>{props.players[0]} <span>vs</span> {props.players[1]}</h1>
       {/* To be shown when there is a winner */}
-      <h2>Joe is the winner by 1!</h2>
+      <h2>{props.winner} is the winner by {props.scoreDifference}! 🏆</h2>
       {/* To be shown when there is no winner */}
       <h2>No winners yet!</h2>
     </article>
   );
-}
+};
+
 export default Match;
