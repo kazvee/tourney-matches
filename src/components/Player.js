23e29a6a7ce50ff02f26217a83a88e8f7e29c6ae
kazvee <kayvee123@gmail.com>
2023-09-01 19:28:51 -0600

    Add conditional rendering to display player total wins

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,12 +4,9 @@ const Player = (props) => {
   return (
     <article className="Player">
       <h1>{props.firstName} <span>{props.gamerTag}</span> {props.lastName}</h1>
-      {/* To be shown when there are no wins */}
-      <h2 className="zero">Currently with no wins ☹️</h2>
-      {/* To be shown when there is 1 win */}
-      <h2>Currently at 1 win</h2>
-      {/* To be shown when there is more than one win */}
-      <h2>Currently at {props.wins} wins</h2>
+      {props.wins === 0 && <h2 className="zero">Currently with no wins ☹️</h2>}
+      {props.wins === 1 && <h2>Currently at {props.wins} win 🙂</h2>}
+      {props.wins > 1 && <h2>Currently at {props.wins} wins 😃</h2>}
     </article>
   );
 };
