2fbd59d89f95d26674700bcd24ccf5399c0209c2
kazvee <kayvee123@gmail.com>
2023-09-01 18:25:22 -0600

    Refactor Player component to receive props to render one player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,19 +1,17 @@
 import React from "react";
 
-const Player = () => {
+const Player = (props) => {
   return (
     <article className="Player">
-      <h1>
-        John <span>Secret</span> Doe
-      </h1>
+      <h1>{props.firstName} <span>{props.gamerTag}</span> {props.lastName}</h1>
       {/* To be shown when there are no wins */}
       <h2 className="zero">Currently with no wins ☹️</h2>
       {/* To be shown when there is 1 win */}
       <h2>Currently at 1 win</h2>
       {/* To be shown when there is more than one win */}
-      <h2>Currently at 1+ wins</h2>
+      <h2>Currently at {props.wins} wins</h2>
     </article>
   );
-}
+};
 
 export default Player;
