fa53709887efde180e4f1c8a5c58e6e8a97e16c0
kazvee <kayvee123@gmail.com>
2023-09-01 19:08:42 -0600

    Add conditional rendering to display the match winner

--- src/components/Match.js
+++ src/components/Match.js
@@ -4,10 +4,12 @@ const Match = (props) => {
   return (
     <article className="Match">
       <h1>{props.players[0]} <span>vs</span> {props.players[1]}</h1>
-      {/* To be shown when there is a winner */}
-      <h2>{props.winner} is the winner by {props.scoreDifference}! 🏆</h2>
-      {/* To be shown when there is no winner */}
-      <h2>No winners yet!</h2>
+      {props.winner ? <h2>{props.winner} is the winner by {props.scoreDifference}! 🏆</h2> : <h2>No winners yet!</h2>}
+
+      {/* Could also use short-circuit evaluation: 
+      {props.winner && <h2>{props.winner} is the winner by {props.scoreDifference}! 🏆</h2>}
+      {!props.winner && <h2>No winners yet!</h2>} */}
+
     </article>
   );
 };
