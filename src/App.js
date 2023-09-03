a47cf67fa1634b21691c9bc826a1bbfaeb80f6e6
kazvee <kayvee123@gmail.com>
2023-09-02 18:05:12 -0600

    Import player helper functions into the App component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import "./App.css";
 import PlayerList from "./components/PlayerList";
 import MatchList from "./components/MatchList";
 import matchData from "./data/matchData";
+import playerData from "./data/playerData";
+import { preparePlayerData, addWinsToPlayers } from "./helpers/playerHelpers";
 
 const App = (props) => {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+
   return (
     <div className="App">
       <h1>
         Tourney Matches{" "}
         <span>Where Coding and Tournaments meet their Match!</span>
       </h1>
-      <PlayerList matchData={matchData} />
+      <PlayerList playerData={parsedPlayerData} />
       <MatchList matchData={matchData} />
     </div>
   );
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Player from "./Player";
-import playerData from '../data/playerData';
-import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 const PlayerList = (props) => {
-
-  const playerDataArray = preparePlayerData(playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, props.matchData);
-  const onePlayer = parsedPlayerData[0];
+  const onePlayer = props.playerData[0];
 
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
+      <Player
+        gamerTag={onePlayer.gamerTag}
+        firstName={onePlayer.firstName}
+        lastName={onePlayer.lastName}
+        wins={onePlayer.wins}
+      />
     </section>
   );
 };
